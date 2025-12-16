5f3d65482348df56e39d103283023ec08446d074
Dengfeng Zheng <zhengdengfeng@gmail.com>
2025-12-16 09:42:07 -0500

    Check both for-loop increment statement and loop body for iterator incrementation

--- clang-plugin-demo/ForLoopIteratorVisitor.cpp
+++ clang-plugin-demo/ForLoopIteratorVisitor.cpp
@@ -5,7 +5,63 @@
 #include "clang/Tooling/Tooling.h"
 #include "llvm/Support/raw_ostream.h"
 
+
 using namespace clang;
+
+// Visitor to check increment operations inside the for loop body
+class ForBodyVisitor : public RecursiveASTVisitor<ForBodyVisitor> {
+public:
+  explicit ForBodyVisitor(const VarDecl *IteratorVar) : IteratorVar(IteratorVar), FoundIncrement(false) {}
+
+  bool VisitUnaryOperator(UnaryOperator *UO) {
+    // ++iterator or iterator++
+    if ((UO->getOpcode() == UO_PreInc || UO->getOpcode() == UO_PostInc)) {
+      if (const DeclRefExpr *DRE = dyn_cast<DeclRefExpr>(UO->getSubExpr())) {
+        if (DRE->getDecl() == IteratorVar) {
+          llvm::outs() << "Iterator increment found in loop body.\n";
+          FoundIncrement = true;
+        }
+      }
+    }
+    return true;
+  }
+
+  bool VisitBinaryOperator(BinaryOperator *BOP) {
+    // iterator += ...
+    if (BOP->getOpcode() == BO_AddAssign) {
+      if (const DeclRefExpr *DRE = dyn_cast<DeclRefExpr>(BOP->getLHS())) {
+        if (DRE->getDecl() == IteratorVar) {
+          llvm::outs() << "Iterator += found in loop body.\n";
+          FoundIncrement = true;
+        }
+      }
+    }
+    // iterator = iterator + ...
+    else if (BOP->getOpcode() == BO_Assign) {
+      if (const DeclRefExpr *LHS = dyn_cast<DeclRefExpr>(BOP->getLHS())) {
+        if (LHS->getDecl() == IteratorVar) {
+          if (const BinaryOperator *RHSBOP = dyn_cast<BinaryOperator>(BOP->getRHS())) {
+            if (RHSBOP->getOpcode() == BO_Add) {
+              if (const DeclRefExpr *RHSLHS = dyn_cast<DeclRefExpr>(RHSBOP->getLHS())) {
+                if (RHSLHS->getDecl() == IteratorVar) {
+                  llvm::outs() << "Iterator = iterator + ... found in loop body.\n";
+                  FoundIncrement = true;
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    return true;
+  }
+
+  bool foundIncrement() const { return FoundIncrement; }
+
+private:
+  const VarDecl *IteratorVar;
+  bool FoundIncrement;
+};
 
 class ForLoopVisitor : public RecursiveASTVisitor<ForLoopVisitor> {
 public:
@@ -19,27 +75,26 @@ public:
           std::string iteratorName = var->getNameAsString();
           llvm::outs() << "Iterator variable: " << iteratorName << "\n";
 
-          // Now check the increment statement
-          const Expr *inc = forStmt->getInc();
-          bool incrementsIterator = false;
-          if (inc) {
+          // Check the increment statement of the for loop
+          bool incStmtIncrementsIterator = false;
+          if (const Expr *inc = forStmt->getInc()) {
             // ++i or i++
             if (const UnaryOperator *uop = dyn_cast<UnaryOperator>(inc)) {
               if ((uop->getOpcode() == UO_PreInc || uop->getOpcode() == UO_PostInc)) {
                 if (const DeclRefExpr *dre = dyn_cast<DeclRefExpr>(uop->getSubExpr())) {
                   if (dre->getDecl() == var) {
-                    incrementsIterator = true;
+                    incStmtIncrementsIterator = true;
                   }
                 }
               }
             }
-            // i += 1 or i = i + 1
+            // i += ... or i = i + ...
             else if (const BinaryOperator *bop = dyn_cast<BinaryOperator>(inc)) {
               // i += ...
               if (bop->getOpcode() == BO_AddAssign) {
                 if (const DeclRefExpr *dre = dyn_cast<DeclRefExpr>(bop->getLHS())) {
                   if (dre->getDecl() == var) {
-                    incrementsIterator = true;
+                    incStmtIncrementsIterator = true;
                   }
                 }
               }
@@ -51,7 +106,7 @@ public:
                       if (rhsBop->getOpcode() == BO_Add) {
                         if (const DeclRefExpr *rhsLHS = dyn_cast<DeclRefExpr>(rhsBop->getLHS())) {
                           if (rhsLHS->getDecl() == var) {
-                            incrementsIterator = true;
+                            incStmtIncrementsIterator = true;
                           }
                         }
                       }
@@ -61,10 +116,19 @@ public:
               }
             }
           }
-          if (incrementsIterator) {
+          if (incStmtIncrementsIterator) {
             llvm::outs() << "Increment statement increments the iterator.\n";
           } else {
             llvm::outs() << "Warning: Increment statement does NOT increment the iterator!\n";
+          }
+
+          // Use a separate visitor to check for increment in the loop body
+          ForBodyVisitor bodyVisitor(var);
+          bodyVisitor.TraverseStmt(forStmt->getBody());
+          if (bodyVisitor.foundIncrement()) {
+            llvm::outs() << "Increment operation found in loop body.\n";
+          } else {
+            llvm::outs() << "Warning: No increment operation found for iterator in loop body!\n";
           }
         }
       }
